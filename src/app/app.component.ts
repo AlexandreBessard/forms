4da0a0a6dac4bdf273e58856e890cb3b3f624072
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-11-08 16:35:59 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // getting access to the form
+  @ViewChild('f')
+  signupForm: NgForm
+
+  defaultQuestion: string = "pet";
+  answer = "";
+  genders = ['male', 'female'];
+
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+/*  onSubmit(form: NgForm) {
+    console.log(form);
+  }*/
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+}
